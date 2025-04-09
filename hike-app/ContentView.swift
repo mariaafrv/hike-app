a61ffa2f0c3e5a82ba602c14c6594a2150b6602e
Maria Aida Fernandes Vitoria <mariaafv@hotmail.com>
2025-04-09 10:58:15 -0300

    Initial Commit

---
+++ hike-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hike-app
+//
+//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 09/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hike-app/hike_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hike_appApp.swift
+//  hike-app
+//
+//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct hike_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
